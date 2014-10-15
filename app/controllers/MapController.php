f26ef27fa131a1c92beea60bdea4e73025676846
John Dapzar <johndapzar@yahoo.com>
2014-10-15 11:46:57 +0530

    123

--- app/controllers/MapController.php
+++ app/controllers/MapController.php
@@ -1,15 +1,14 @@
 <?php  
 
 	class MapController extends \BaseController {
-		public function getLatlong($id)
+		public function getLatlong()
 		{
+			$id = Input::get('id');
+			$lat=Tubewell::where('tubewell_code','=',$id)->select('latitude','longitude')->first();
+			/*$longi=Tubewell::where('tubewell_code','=',$id)->select('longitude')->first();
 
-
-			$lat=Tubewell::where('tubewell_code','=',$id)->select('latitude')->first();
-			$longi=Tubewell::where('tubewell_code','=',$id)->select('latitude')->first();
-
-			$arrvalue=array($lat,$longi);
-			
+			*/
+			$arrvalue=array($lat->latitude,$lat->longitude);
 			echo json_encode($arrvalue);
 		}
 	}
