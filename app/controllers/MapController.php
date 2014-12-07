a0c34a0902795c76b7f0a403644625eab8abea31
Chhama Chenkual <chhama5@gmail.com>
2014-12-07 21:56:21 +0530

    map cum excel

--- app/controllers/MapController.php
+++ app/controllers/MapController.php
@@ -4,12 +4,21 @@
 		public function getLatlong()
 		{
 			$id = Input::get('id');
-			$lat=Tubewell::where('tubewell_code','=',$id)->select('latitude','longitude')->first();
+			$lat=Tubewell::where('tubewell_code','=',$id)->first();
 			$water=WaterQuality::where('tubewell_id','=',$id)->first();
+			$delivery=Delivery::where('id','=',$lat->delivery_id)->first();
+			$district=District::where('id','=',$lat->district_id)->first();
+			$block=Block::where('id','=',$lat->block_id)->first();
+			$panchayat=Panchayat::where('id','=',$lat->panchayat_id)->first();
+			$zone=OfficeZone::where('id','=',$lat->office_zone_id)->first();
+			$circle=OfficeCircle::where('id','=',$lat->office_circle_id)->first();
+			$division=OfficeDivision::where('id','=',$lat->office_division_id)->first();
+			$subdivision=OfficeSubDivision::where('id','=',$lat->office_sub_division_id)->first();
+			$section=OfficeSection::where('id','=',$lat->office_section_id)->first();
 			/*$longi=Tubewell::where('tubewell_code','=',$id)->select('longitude')->first();
 
 			*/
-			$arrvalue=array($lat->latitude,$lat->longitude,$water->ph,$water->colour,$water->odour);
+			$arrvalue=array($lat->latitude,$lat->longitude,$lat->tubewell_code,$lat->location,$lat->sub_location,$lat->landmark,$lat->elevation,$lat->depth_swl,$lat->depth_boring,$lat->size_boring,$lat->drilling_date,$lat->commission_date,$lat->discharge,$lat->platform,$lat->well_status,$lat->well_status_date,$lat->well_status_note,$lat->well_status_nature_damage,$lat->well_status_action,$lat->well_status_repaired_date,$lat->well_status_repaired_by,$delivery->name,$district->name,$block->name,$panchayat->name,$zone->name,$circle->name,$division->name,$subdivision->name,$section->name);
 			echo json_encode($arrvalue);
 		}
 	}
--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -31,14 +31,40 @@
 				 data: {'id' : data},
 				 type: 'GET',
 				success: function(data){
-
-					$('#tubewell_info').html("<table cellpadding=5 class='table table-striped' style='width:200px'><tr><td>ph</td><td>"
-					        +data[2]+"</tr><tr><td>Colour</td><td>"
-					        +data[3]+"</td></tr><tr><td>Odour</td><td>"
-					        +data[4]+"</td></tr><tr><td>Taste</td><td>"
-					        +data[5]+"</td></tr></table>");
-
 					var data=JSON.parse(data);
+
+					$('#tubewell_info').html("<div class='panel panel-info'><div class='panel-heading'>Tubewell Information</div><div class='panel-body'><table cellpadding=5 class='table table-striped table-hover' style='width:200px'><tr><td><strong>Tubewell Code</td><td>"
+					        +data[2]+"<td><strong>Location</strong></td><td>"
+					        +data[3]+"</td><td><strong>Sub-Location</td><td>"
+					        +data[4]+"</td><td><strong>Landmark</td><td>"
+					        +data[5]+"</td><td><strong>Latitude</td><td>"
+					        +data[0]+"</td><td><strong>Longitude</td><td>"
+					        +data[1]+"</td><td><strong>Elevation</td><td>"
+					        +data[6]+"</td><td><strong>Depth of SWL</td><td>"
+					        +data[7]+"</td><td><strong>Depth of Boring</td><td>"
+					        +data[8]+"</td><td><strong>Size of Boring</td><td>"
+					        +data[9]+"</td></tr><tr><td><strong>Drilling Date</td><td>"
+					        +data[10]+"</td><td><strong>Commission Date</td><td>"
+					        +data[11]+"</td><td><strong>Discharge</td><td>"
+					        +data[12]+"</td><td><strong>Platform?</td><td>"
+					        +data[13]+"</td><td><strong>Tubewell Status</td><td>"
+					        +data[14]+"</td><td><strong>Tubewell Status Date</td><td>"
+					        +data[15]+"</td><td><strong>Note</td><td>"
+					        +data[16]+"</td><td><strong>Nature of Damage</td><td>"
+					        +data[17]+"</td><td><strong>Action</td><td>"
+					        +data[18]+"</td><td><strong>Repaired Date</td><td>"
+					        +data[19]+"</td></tr><tr><td><strong>Repaired by</td><td>"
+					        +data[20]+"</td><td><strong>Mode of Delivery</td><td>"
+					        +data[21]+"</td><td><strong>District</td><td>"
+					        +data[22]+"</td><td><strong>Block</td><td>"
+					        +data[23]+"</td><td><strong>Panchayat</td><td>"
+					        +data[24]+"</td><td><strong>Zone</td><td>"
+					        +data[25]+"</td><td><strong>Circle</td><td>"
+					        +data[26]+"</td><td><strong>Division</td><td>"
+					        +data[27]+"</td><td><strong>Sub-Division</td><td>"
+					        +data[28]+"</td><td><strong>Section</td><td>"
+     						+data[29]+"</td></tr></table></div></div>");
+
 			        // alert(data);
 			        myCenter=  new google.maps.LatLng(data[0],data[1]);
 					var mapProp = {
@@ -58,11 +84,39 @@
 					marker.setMap(map);
 
 					var infowindow = new google.maps.InfoWindow({
-					        content: "<table cellpadding=5 class='table table-striped' style='width:200px'><tr><td>ph</td><td>"
-					        +data[2]+"</tr><tr><td>Colour</td><td>"
-					        +data[3]+"</td></tr><tr><td>Odour</td><td>"
-					        +data[4]+"</td></tr><tr><td>Taste</td><td>"
-					        +data[5]+"</td></tr></table>"
+					        content: "<table cellpadding=5 class='table table-striped table-hover' style='width:200px'><tr><td><strong>Tubewell Code</td><td>"
+					        +data[2]+"<td><strong>Location</strong></td><td>"
+					        +data[3]+"</td><td><strong>Sub-Location</td><td>"
+					        +data[4]+"</td><td><strong>Landmark</td><td>"
+					        +data[5]+"</td></tr><tr><td><strong>Latitude</td><td>"
+					        +data[0]+"</td><td><strong>Longitude</td><td>"
+					        +data[1]+"</td><td><strong>Elevation</td><td>"
+					        +data[6]+"</td><td><strong>Depth of SWL</td><td>"
+					        +data[7]+"</td></tr><tr><td><strong>Depth of Boring</td><td>"
+					        +data[8]+"</td><td><strong>Size of Boring</td><td>"
+					        +data[9]+"</td><td><strong>Drilling Date</td><td>"
+					        +data[10]+"</td><td><strong>Commission Date</td><td>"
+					        +data[11]+"</td></tr><tr><td><strong>Discharge</td><td>"
+					        +data[12]+"</td><td><strong>Platform?</td><td>"
+					        +data[13]+"</td><td><strong>Tubewell Status</td><td>"
+					        +data[14]+"</td><td><strong>Tubewell Status Date</td><td>"
+					        +data[15]+"</td></tr><tr><td><strong>Note</td><td>"
+					        +data[16]+"</td><td><strong>Nature of Damage</td><td>"
+					        +data[17]+"</td><td><strong>Action</td><td>"
+					        +data[18]+"</td><td><strong>Repaired Date</td><td>"
+					        +data[19]+"</td></tr><tr><td><strong>Repaired by</td><td>"
+					        +data[20]+"</td><td><strong>Mode of Delivery</td><td>"
+					        +data[21]+"</td><td><strong>District</td><td>"
+					        +data[22]+"</td><td><strong>Block</td><td>"
+					        +data[23]+"</td></tr><tr><td><strong>Panchayat</td><td>"
+					        +data[24]+"</td><td><strong>Zone</td><td>"
+					        +data[25]+"</td><td><strong>Circle</td><td>"
+					        +data[26]+"</td><td><strong>Division</td><td>"
+					        +data[27]+"</td></tr><tr><td><strong>Sub-Division</td><td>"
+					        +data[28]+"</td><td><strong>Section</td><td>"
+     						+data[29]+"</td></tr></table>"
+
+		
 
 					        				
 
@@ -127,11 +181,11 @@
 		
 
 	</div>
-	<div class="col-md-9"></div>
-
-	<div class="col-md-3" id='tubewell_info'>
+	<div class="row"></div>
+	<div class="col-md-12" id='tubewell_info' style="margin-top:20px;">
 		
-	</div>
+			
+</div>
 
 
 @stop
